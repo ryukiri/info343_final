fea3085d2bbf54fdf26f38d7c70eaae13661b05a
Austin Quach <austinqq@uw.edu>
2017-11-27 14:17:02 -0800

    Allowed events to show up

---
+++ challenge9/src/Search.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    render() {
+        return (
+            <div>
+                <form
+                    onSubmit={(e) => {
+                        this.handleFormSubmit(e);
+                    }}
+                >
+                    <i className="material-icons">search</i>
+                    <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <input ref="inputBox" className="mdl-textfield__input" type="text" id="sample3"/>
+                        <label className="mdl-textfield__label" htmlFor="sample3">Search</label>
+                    </div>
+                    <a>
+                        <button type="submit"><i className="material-icons">send</i></button>
+                    </a>
+                </form>
+            </div>
+        );
+    }
+ 
+    handleFormSubmit(e) {
+        e.preventDefault();
+ 
+        var item = this.refs.inputBox.value;
+ 
+        this.props.onFormSubmit(item);
+    }
+ }
+ 
+ export default Search;
---
+++ challenge9/src/map.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import SimpleMap from './SimpleMap.js';
+
+ReactDOM.render(<SimpleMap />, document.getElementById('root'));
